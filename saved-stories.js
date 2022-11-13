487827491a0924a95acb8e218930537f8657762b
vren1 <victoriaren7@gmail.com>
2022-11-12 22:46:49 -0600

    added
    added for you page

---
+++ saved-stories.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+    createCard('Test Title', 'English', 'Advanced', 'This is a test');
+})
+
+function createCard(name, language, level, contentPreview){
+
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.setAttribute('width', '18rem');
+
+    const cardBody = document.createElement('div');
+    const cardTitle = document.createElement('h5');
+    cardTitle.textContent = name;
+    const cardSubtitle = document.createElement('h6');
+    cardSubtitle.className = 'card-subtitle mb2 text-muted';
+    cardSubtitle.textContent = `${level} ${language}`;
+
+    const cardText = document.createElement('p');
+    cardText.textContent = contentPreview;
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardSubtitle);
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody);
+
+    document.body.appendChild(card);
+
+    console.log('card created');
+}
